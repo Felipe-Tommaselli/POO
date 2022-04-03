2c45e9c80bcf47af91aefd4f1210f578e481c701
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-04-03 19:23:47 -0300

    Comentando o código

--- Lista1/src/exec4_2.java
+++ Lista1/src/exec4_2.java
@@ -5,7 +5,10 @@
 public class exec4_2 {
 
     public static void main(String[] args) {
+        // cabeçalho
         System.out.println("EXERCICIO 4 (parte 2): Verificador de Primo\n");
+
+        // validação da entrada num (análogo ao feito em aula)
         int num = 0;
         boolean leu = false;
         while(!leu){
@@ -23,15 +26,16 @@ public class exec4_2 {
             }
         }
         // com num validado, vamos ver se ele é primo
-        
         boolean primo = true;
         int i;
+        // teste de divisão (i guarda o menor divisor comum caso haja)
         for(i = 2; i < num; i++) {
             if (num % i == 0){
                 primo = false;
                 break;
             }
         }
+        // resposta para primo ou não
         if(primo == true)
             System.out.printf("O número %d é primo!\n", num);
         else{
