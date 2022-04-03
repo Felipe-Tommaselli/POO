20d2d6eb3f9f53b53af101f58454215e5cb1c552
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-04-02 22:45:54 -0300

    exec 3 e 4 funcionando bala

--- Lista1/src/exec3.java
+++ Lista1/src/exec3.java
@@ -3,5 +3,32 @@
  * @author 11800910
  */
 public class exec3 {
-    
+
+    public static void main(String[] args) {
+        System.out.println("EXERCICIO 3: Arvore esquerda->direita\n");
+        int num = 0;
+        boolean leu = false;
+        while(!leu){
+            leu = true;
+            System.out.printf("Digite um inteiro: ");
+            try {
+                num = EntradaTeclado.leInt();
+                if( num < 0){
+                    System.out.println("Número deve ser positivo!!");
+                    leu = false;
+                }
+            } catch (Exception e) {
+            System.out.println("O valor que vc digitou não é um número inteiro");
+                leu = false;
+            }
+        }
+        // com num validado, vamos montar a arvore
+        int x = num;
+        while(x > 0){
+            for(int i = 0; i < x; i++)
+                System.out.printf("*");
+            System.out.printf("\n");
+            x--;
+        }
+    }
 }
--- Lista1/src/exec4.java
+++ Lista1/src/exec4.java
@@ -3,5 +3,37 @@
  * @author 11800910
  */
 public class exec4 {
-    
+
+    public static void main(String[] args) {
+        System.out.println("EXERCICIO 3: Arvore esquerda->direita\n");
+        int num = 0;
+        boolean leu = false;
+        while(!leu){
+            leu = true;
+            System.out.printf("Digite um inteiro: ");
+            try {
+                num = EntradaTeclado.leInt();
+                if( num < 0){
+                    System.out.println("Número deve ser positivo!!");
+                    leu = false;
+                }
+            } catch (Exception e) {
+            System.out.println("O valor que vc digitou não é um número inteiro");
+                leu = false;
+            }
+        }
+        // com num validado, vamos montar a arvore
+        int asterisco = num;
+        int espaco = 0;
+        while(asterisco > 0){
+            for(int i = 0; i < espaco; i++)
+                System.out.printf(" ");
+            for(int i = 0; i < asterisco; i++)
+                System.out.printf("*");
+            System.out.printf("\n");
+            asterisco--;
+            espaco++;
+        }
+    }
+
 }
