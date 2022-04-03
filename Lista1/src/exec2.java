f68166e9818d1b24b09666153bb41dcec4425f9f
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-04-03 19:15:59 -0300

    Comentando o código

--- Lista1/src/exec2.java
+++ Lista1/src/exec2.java
@@ -8,17 +8,22 @@ import java.lang.Math;
 public class exec2 {
 
     public static void main(String[] args) {
+        // cabeçalho
         System.out.println("EXERCICIO 2: EQUAÇÃO SEGUNDO GRAU\n");
+        
         int num = 0;
         int i = 1;
         boolean leu = false;
         int a = 0;
         int b = 0;
         int c = 0;
+        // loop para repetir o processo para os 3 coeficientes a,b,c tal que:
+        // ax^2 + bx + c = 0
         while(i <= 3){
             leu = false;
             num = 0;
-            while(!leu){
+        // validação da entrada num (análogo ao feito em aula)
+        while(!leu){
                 leu = true;
                 System.out.printf("Digite o %dº coeficiente: ", i);
                 try {
@@ -32,6 +37,7 @@ public class exec2 {
                     leu = false;
                 }
             }
+            // atribui o numero validado para cada iteração
             if(i == 1)
                 a = num;
             if(i == 2)
@@ -44,16 +50,22 @@ public class exec2 {
         float x = 0;
         float delta = 0;
 
+        // calculo do discriminante
         delta = (b*b) - (float) 4.0*a*c;
         System.out.printf("delta = %f\n", delta);
         
+        // para cada caso do discriminante, a equação terá diferentes soluções
+        // a partir disso, é aplicada a fórmula de baskhara
+        // SEM RAIZES REAL
         if(delta < 0)
             System.out.printf("Equação fornecida não possui soluçaõ :(\n");
-        else if(delta == 0){
+        // UMA RAIZ REAL
+            else if(delta == 0){
             x = (float)-b;
             x /= (float)(2.0*a);
             System.out.printf("Equação fornecida tem uma solução: %f\n", x);
         } 
+        // DUAS RAIZES REAIS
         else{
             x = (float)(-b + Math.sqrt(delta));
             x /= (float)(2.0*a);
