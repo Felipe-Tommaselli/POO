bb2534201baf555e709dc687862bc51500e96b98
Felipe-Tommaselli <f.tommaselli@usp.br>
2022-04-03 23:50:16 -0300

    funcionando

--- Lista1/src/exec6.java
+++ Lista1/src/exec6.java
@@ -4,4 +4,41 @@
  */
 public class exec6 {
     
+    public static void main(String[] args) {
+        // cabeçalho
+        System.out.println("EXERCICIO 6: Valores ponto fluante\n");
+
+        // validação da entrada num (análogo ao feito em aula)
+        // com inserção em um vetor dos valores
+        double num = 0.0;
+        boolean leu = false;
+        double nums[] = new double[999];
+        int i = 0;
+        loopfora:
+        while(true){
+            num = 0.0;
+            while(!leu){
+                leu = true;
+                System.out.printf("Digite um inteiro: ");
+                try {
+                    num = EntradaTeclado.leDouble();
+                    if(num == 0.0)
+                        break loopfora;
+                } catch (Exception e) {
+                System.out.println("O valor que vc digitou não é um número");
+                    leu = false;
+                }
+            }
+            nums[i] = num;
+            i++;
+        }
+        // com num validado, vamos procurar o proximo menor num
+        while(i<9){
+            System.out.printf("nums[%d]: %f\n", i, nums[i]);
+            i++;
+        }
+        // resposta
+        System.out.printf("O primeiro menor primo depois \n");
+
+    }
 }
--- Lista1/src/exec7.java
+++ Lista1/src/exec7.java
@@ -2,6 +2,72 @@
  *
  * @author 11800910
  */
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.Math;
+
 public class exec7 {
-    
+
+    public static void main(String[] args) {
+        // cabeçalho
+        System.out.println("EXERCICIO 7: Bisseção\n");
+
+        // validação da entrada (análogo ao feito em aula)
+        boolean leu = false;
+        double a0 = 0.0;
+        double b0 = 0.0;
+        double erro = 0, intervalo, m, i;
+        int div = 0;
+        while(!leu){
+            leu = true;
+            System.out.println("Digite um número: ");
+            try{
+                a0 = EntradaTeclado.leDouble();
+            } catch(Exception e){
+                System.out.println("O valor digitado deve ser um número!");
+                leu = false;
+            }
+        }
+        leu = false;
+        while(!leu){
+            leu = true;
+            System.out.println("Digite um número: ");
+            try{
+                b0 = EntradaTeclado.leDouble();
+            } catch(Exception e){
+                System.out.println("O valor digitado deve ser um número!");
+                leu = false;
+            }
+        }
+        // com num validado, vamos procurar o proximo menor num
+        intervalo = b0 - a0;
+        div = ((Math.pow(a0, 3) - Math.pow(a0, 2) - 13*a0 + 8) * (Math.pow(b0, 3) - Math.pow(b0, 2) - 13*b0 + 8) > 0 ? 0 : 1); 
+        i = Math.log10(b0 - a0);
+        i -= Math.log10(erro);
+        i /= Math.log(2);
+        if(((Math.pow(a0, 3) - Math.pow(a0, 2) - 13*a0 + 8)) == 0 || (Math.pow(b0, 3) - Math.pow(b0, 2) - 13*b0 + 8) == 0){
+            if(((Math.pow(a0, 3) - Math.pow(a0, 2) - 13*a0 + 8)) == 0)
+                System.out.printf("a0 = %f\n", a0);
+            else
+                System.out.printf("b0 = %f\n", b0);
+        }
+        while(erro > 0.0000001){
+            while(intervalo > erro){
+                intervalo = b0 - a0;
+                m = (a0 + b0) / 2;
+            if(((Math.pow(m, 3) - Math.pow(m, 2) - 13*m + 8)) == 0){
+                System.out.printf("m = %f\n", m);
+                break;
+            }
+            if(((Math.pow(a0, 3) - Math.pow(a0, 2) - 13*a0 + 8)) * (Math.pow(m, 3) - Math.pow(m, 2) - 13*m + 8) < 0){
+                b0 = m;
+            }
+            else{
+                a0 = m;
+            }
+            }
+        }
+        System.out.printf("[%f, %f]\n solução: %f\n", a0, b0, (a0+b0)/2);
+    }
 }
