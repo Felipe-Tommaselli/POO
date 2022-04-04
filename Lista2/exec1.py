435061bb51859953b9a20be89133a2e65a109278
Felipe Tommaselli <f.tommaselli@usp.br>
2022-04-04 09:01:07 -0300

    feitos, não testados ainda

--- Lista2/exec1.py
+++ Lista2/exec1.py
@@ -3,7 +3,38 @@
  * @author 11800910
  */
 
+def valida_entrada():
+    while(True):
+        try:
+            num = int(input('"Digite um inteiro: '))
+            break
+        except ValueError:
+            print('O valor que vc digitou não é um número inteiro')
+    return num
+
 def raiz_quadrada():
     
 
 if __name__ == '__main__':
+
+    # cabeçalho
+    print('EXERCICIO 1: RAIZ QUADRADA\n')
+    
+    # valdação da entrada 
+    num = valida_entrada()
+
+    # com num validado, vamos calcular a raiz
+    xi = 0
+    xi_1 = num/2 
+    erro = None
+
+    # fórmula iterativa da raiz quadrada com erro do enunciado
+    while(erro > 0.00000001):
+        xi = (xi_1 + (num/xi_1))/2
+        erro = xi - xi_1
+        if(erro < 0):
+            erro *= -1
+        xi_1 = xi
+
+    # resultado
+    print(f'"A raiz quadrada de {num} é {xi}')
--- Lista2/exec2.py
+++ Lista2/exec2.py
@@ -3,7 +3,50 @@
  * @author 11800910
  */
 
+import math
+
+def valida_entrada():
+    while(True):
+        try:
+            num = int(input('"Digite um inteiro: '))
+            break
+        except ValueError:
+            print('O valor que vc digitou não é um número inteiro')
+    return num
+
 def raiz_quadrada():
     
 
 if __name__ == '__main__':
+
+    # cabeçalho
+    print('EXERCICIO 2: EQUAÇÃO SEGUNDO GRAU\n')
+    
+    # valdação da entrada 
+    a0 = valida_entrada()
+    b0 = valida_entrada()
+    c0 = valida_entrada()
+
+    # com num validado, vamos calcular a solução
+    delta = 0
+    x = 0
+
+    # calculo do discriminant
+    delta = (b*b) - 4.0*a*c
+    print("delta =", delta)
+
+    # para cada caso do discriminante, a equação terá diferentes soluções
+    # a partir disso, é aplicada a fórmula de baskhara
+    #* SEM RAIZES REAL
+    if(delta < 0):
+        print('Equação fornecida não possui soluçaõ :(')
+    #* UMA RAIZ REAL
+    elif(delta == 0):
+        x = (-b)/(2.0*a)
+        print('Equação fornecida tem uma solução:', x)
+    #* DUAS RAIZES REAIS
+    else:
+        x = (-b + math.sqrt(delta))/(2.0*a)
+        print('Equação fornecida tem duas soluções:', x, end="")
+        x = (-b - math.sqrt(delta))/(2.0*a)
+        print('e', x)
