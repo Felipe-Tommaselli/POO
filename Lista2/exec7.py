4d17982a774420b695a34d0da351d09e7413f470
Felipe Tommaselli <f.tommaselli@usp.br>
2022-04-04 15:10:17 -0300

    faltando 7 e o 8

--- Lista2/exec7.py
+++ Lista2/exec7.py
@@ -3,7 +3,27 @@
  * @author 11800910
 '''
 
-def raiz_quadrada():
-    
+def valida_entrada():
+    while(True):
+        try:
+            num = int(input('Digite um inteiro: '))
+            if num < 0:
+                print('Número deve ser positivo!!')
+                continue
+            break
+        except ValueError:
+            print('O valor que vc digitou não é um número inteiro')
+    return num
 
 if __name__ == '__main__':
+
+    # cabeçalho
+    print('EXERCICIO 7: Método da Bisseção\n')
+    
+    # valdação da entrada 
+    num = valida_entrada()
+
+    # com num validado, vamos procurar o proximo menor num
+
+    # resposta
+    print(f'O primeiro menor primo depois de {num} é {menor_primo}')
