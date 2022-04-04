6feb9703843d2a5818bd1bec2563f094dcafb7c1
Felipe Tommaselli <f.tommaselli@usp.br>
2022-04-04 15:08:45 -0300

    Código feito, sem testar ainda

--- Lista2/exec6.py
+++ Lista2/exec6.py
@@ -3,7 +3,36 @@
  * @author 11800910
 '''
 
-def raiz_quadrada():
-    
+def valida_entrada():
+    while(True):
+        try:
+            num = int(input('Digite um inteiro: '))
+            break
+        except ValueError:
+            print('O valor que vc digitou não é um número inteiro')
+    return num
 
 if __name__ == '__main__':
+
+    # cabeçalho
+    print('EXERCICIO 6: Valores ponto fluante\n')
+    
+    # validação da entrada passando os resultados para a lista 
+    lista = list()
+    while(True):
+        num = valida_entrada()
+        if num == 0:
+            break
+        lista.append(num)
+
+    # calcula o maior e o menor presentes na lista
+    menor = lista[0]
+    maior = lista[0]
+    for _ in lista:
+        if _ < menor:
+            menor = i
+        if _ > maior:
+            maior = i
+
+    # resposta
+    print(f'O maior número digitado foi {maior} e o menor {menor}')
