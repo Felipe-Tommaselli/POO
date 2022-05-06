a8a198352df5802de88037ec7a120e84f59d1250
Felipe Tommaselli <f.tommaselli@usp.br>
2022-05-06 12:10:44 -0300

    esboço zero

--- Poker/main.py
+++ Poker/main.py
@@ -4,3 +4,26 @@
 * @author: 11800910
 
 '''
+
+class TelePoker():
+
+    def __init__(self):
+        print(self)
+
+
+
+class Cartas():
+
+    num = 5
+    nipe = 'W'
+
+    def printCartas(num, nipe):
+        str =  '+-----+'
+        str += '|     |'
+        str += f'|  {num} {nipe}  |'
+        str += '|     |'
+        str +=  '+-----+'
+
+
+if __name__ == '__main__':
+    print('classe main')
