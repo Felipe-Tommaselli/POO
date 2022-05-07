6b0de7c5406f18da03865ac27f415cc6683e1bf8
Felipe Tommaselli <f.tommaselli@usp.br>
2022-05-07 19:44:35 -0300

    pontuacao funcionandoooo

--- Poker/Pontos.py
+++ Poker/Pontos.py
@@ -5,64 +5,64 @@ from Jogo import *
 class Pontos(object):
 
     def __init__(self, saldo, aposta, cartas: list()):
-        self.premio = 0
         self.saldo = saldo
-        self.aposta = aposta
         self.cartas = cartas
 
-        if rsf == True:
-            self.premio = 200*self.aposta
-        elif sf == True:
-            self.premio = 100*self.aposta
-        elif quadra == True:
-            self.premio = 50*self.aposta
-        elif fh == True:
-            self.premio = 20*self.aposta
-        elif flush == True:
-            self.premio = 10*self.aposta
-        elif s() == True:
-            self.premio = 5*self.aposta
-        elif trinca() == True:
-            self.premio = 2*self.aposta
-        elif doispares() == True:
-            self.premio = self.aposta
+        ganho = 0
+        if self.rsf() == True:
+            ganho = 200
+        elif self.sf() == True:
+            ganho = 100
+        elif self.quadra() == True:
+            ganho = 50
+        elif self.fh() == True:
+            ganho = 20
+        elif self.flush() == True:
+            ganho = 10
+        elif self.s() == True:
+            ganho = 5
+        elif self.trinca() == True:
+            ganho = 2
+        elif self.doispares() == True:
+            ganho = 1
         else:
-            self.premio = 0
+            ganho = 0
         
-        self.saldo += self.premio
+        print("GANHO: ", ganho)
+        self.saldo += ganho*aposta
 
     def rsf(self):
-        if s() == True:
-            if flush() == True:
+        if self.s() == True:
+            if self.flush() == True:
                 for e in self.cartas:
-                    if e[0] not in ('10', 'J', 'Q', 'K', 'A'):
-                        return False
+                    if e[0] in ('2', '3', '4', '5', '6', '7', '8', '9'):
+                        return True
 
-        return True
+        return False
 
     def sf(self):
-        if s() == True:
-            if flush() == True:
+        if self.s() == True:
+            if self.flush() == True:
                 return True
 
         return False
 
     
     def quadra(self):
-        for i in self.cartas:
-            for j in self.cartas:
-                for k in self.cartas:
-                    for l in self.cartas:
-                        if i == j and i == k and i == l:
-                            return True
+
+        nums = [e[0] for e in self.cartas]
+        for num in nums:
+            if nums.count(num) == 4:
+                return True
+
+        return False
 
     def fh(self):
-        if trinca() == True:
-            for i in self.cartas:
-                # procura um par
-                for j in self.cartas:
-                    if i is j:
-                        return True
+        if self.trinca() == True:
+            nums = [e[0] for e in self.cartas]
+            for num in nums:
+                if nums.count(num) == 2:
+                    return True
         
         return False
 
@@ -80,7 +80,16 @@ class Pontos(object):
     def s(self):
         nums = list()
         for e in self.cartas:
-            nums.append(e[0])
+            if e[0] == 'J':
+                nums.append(11)
+            elif e[0] == 'Q':
+                nums.append(12)
+            elif e[0] == 'K':
+                nums.append(13)
+            elif e[0] == 'A':
+                nums.append(14)
+            else:
+                nums.append(int(e[0]))
         
         sortnums = sorted(nums)        
         if sortnums[4]-sortnums[3]==1 and sortnums[3]-sortnums[2]==1 and sortnums[2]-sortnums[1]==1 and sortnums[1]-sortnums[0]==1:
@@ -89,33 +98,28 @@ class Pontos(object):
         return False
 
     def trinca(self) -> bool:
-        # procura uma trinca
-        for i in self.cartas:
-                for j in self.cartas:
-                    for k in self.cartas:
-                        if i is j and i is k :
-                            return True
+        nums = [e[0] for e in self.cartas]
+        for num in nums:
+            if nums.count(num) == 3:
+                return True
 
         return False
 
     def doispares(self) -> bool:
         par0 = False
-        par1 = False
-        for i0 in self.cartas:
-            # procura um par
-            if par0 == False:
-                for j in self.cartas:
-                    if i0 is j:
-                        par0 = True
-                        break
-        for i1 in self.cartas:
-            # procura outro par
-            if par1 == False:
-                for k in self.cartas:
-                    if k == i1 and k != j:
-                        par1 = True
-                        break
-        if par1 == True:
-            return True
-        else:
-            return False
+        
+        nums = [e[0] for e in self.cartas]
+        for num in nums:
+            if nums.count(num) == 2:
+                par0 = True
+                break
+
+        if par0 == True:
+            nums.remove(num)
+            nums.remove(num)
+            for num2 in nums:
+                if nums.count(num2) == 2:
+                    return True
+        
+        return False
+
