c58c44280f3b031028c620e11b95ff62138a4ef8
Felipe Tommaselli <f.tommaselli@usp.br>
2022-05-30 10:45:12 -0300

    ajustando a entrada de dados pelos grupos (falta tratar exceções)

--- Verificar_grupos/data.py
+++ Verificar_grupos/data.py
@@ -6,39 +6,66 @@ import os
 # * É NECESSÁRIO TER A PASTA 'ASSETS' PARA CONSEGUIR OBTER OS DADOS
 path = os.getcwd() + '\\assets\\'
 class data:
-    def getfiles(dir):
+    def getFiles(dir: str):
         relpath = path + dir
         for file in os.listdir(relpath):
             if os.path.isfile(os.path.join(relpath, file)):
                 yield file
-    
-    # falta abrir as duas turmas 
+
     @staticmethod
     def getTurmaList():
-
-        turma = list()
-        with open(file, encoding='utf-8') as ficheiro:
-            reader = csv.reader(ficheiro)        
-            try:
-                for linha in reader:
-                    turma.append(linha)
-            except csv.Error as e:
-                sys.exit('ficheiro %s, linha %d: %s' % (file, reader.line_num, e))
-        return turma
+        turmas = [list(), list()] # duas turmas
+        pos = 0
+        for file in data.getFiles(r''):
+            with open(path + file, encoding='utf-8') as ficheiro:
+                reader = csv.reader(ficheiro)        
+                try:
+                    for linha in reader:
+                        turmas[pos].append(linha)
+                except csv.Error as e:
+                    sys.exit('ficheiro %s, linha %d: %s' % (file, reader.line_num, e))
+            pos += 1
+        
+        return turmas
 
     @staticmethod
-    def getTurmaInfo(fname: str):
-        turma = data.getTurmaList(fname)
+    def getTurmaInfo():
+        turmas = data.getTurmaList()
         
-        nomes = list()
-        nusp = list()
-        for linha in turma:
-            nusp.append(int(linha[0]))
-            nomes.append(linha[1])
+        pos = 0
+        nusp  = [list(), list()]
+        nomes = [list(), list()]
+        for turma in turmas:
+            for linha in turma:
+                nusp[pos].append(int(linha[0]))
+                nomes[pos].append(linha[1])
+            pos += 1
         return nusp, nomes
 
     @staticmethod
-    def getGruposList(fname: str):
-        print('hello')
+    def getGruposList():
+        dirpath = path + r'SSC0103-2022-Definição dos grupos-4240978\\'
+        res = list()
+        for relpath in os.listdir(dirpath):
+            if os.path.isfile(os.path.join(dirpath, relpath)): # check if current path is a file
+                res.append(relpath) 
+
+        print(res)
+        grupos = [list() for i in range(len(res))]
+        pos = 0
+
+        # aporra do arquivo do joa nao ta em utf, mas oresto ta funcionado
+        for file in data.getFiles(r'SSC0103-2022-Definição dos grupos-4240978\\'):
+            print(file)
+            with open(dirpath + file, encoding='utf-8') as ficheiro:
+                reader = csv.reader(ficheiro)        
+                try:
+                    for linha in reader:
+                        grupos[pos].append(linha)
+                except csv.Error as e:
+                    sys.exit('ficheiro %s, linha %d: %s' % (file, reader.line_num, e))
+            pos += 1
+        
+        return grupos
 
 
--- Verificar_grupos/main.py
+++ Verificar_grupos/main.py
@@ -1,15 +1,11 @@
 from data import *
 
-path = os.getcwd() + '\\assets\\'
 
-for file in getfiles('\\SSC0103-2022-Definição dos grupos-4240978'):
-    print(file)
-turma1 = data.getTurmaList('ListadeApoioaoDocente-SSC01032022101.csv')
-turma2 = data.getTurmaList('ListadeApoioaoDocente-SSC01032022102.csv')
+turmas = data.getTurmaList()
+nusp, nomes = data.getTurmaInfo()
+grupos = data.getGruposList()
 
-nusp1, nomes1 = data.getTurmaInfo('ListadeApoioaoDocente-SSC01032022101.csv')
-
-print(turma1)
-
-print(nusp1)
-print(nomes1)
+print(turmas[0])
+print('\n\n')
+print(nusp[0])
+print(nomes[0])
